b8e23bf993f277175d24ce162c178db79531c6d6
Philipe Coutinho <philipecoutinhor@gmail.com>
2026-05-20 23:00:57 -0300

    Use card only for project Stripe checkout

--- supabase/functions/stripe-create-payment/index.ts
+++ supabase/functions/stripe-create-payment/index.ts
@@ -151,7 +151,7 @@ serve(async (req) => {
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
       customer_email: customerId ? undefined : user.email,
-      payment_method_types: ['card', 'boleto', 'pix'],
+      payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
